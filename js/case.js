5f3e33e4b527ab9ae3adbecb8eff21ea60224e6d
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-15 18:11:37 +0600

    added js and html code edit

--- js/case.js
+++ js/case.js
@@ -29,13 +29,13 @@ function  updateCaseTotalPrice(newCaseNumber){
 
 document.getElementById('btn-case-plus').addEventListener('click', function(){
    const newCaseNumber = updateCaseNumber(true);
-
     updateCaseTotalPrice(newCaseNumber);
+    calculateSubTotal();
 });
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
-
    const newCaseNumber = updateCaseNumber(false);
+   calculateSubTotal();
 
 
     updateCaseTotalPrice(newCaseNumber);
