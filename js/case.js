2900969f15c8eb858dbcaa82dc2f4bb97ad31584
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-15 11:55:30 +0600

    html and css complete , added some function

---
+++ js/case.js
@@ -0,0 +1,19 @@
+
+//1) add event listener to the case plus button,
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+   const caseNumberField = document.getElementById('case-number-field');
+   const caseNumberString = caseNumberField.value;
+   const previousCaseNumber = parseInt(caseNumberString);
+
+   const newCaseNumber = previousCaseNumber + 1;
+   caseNumberField.value = newCaseNumber;
+});
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber  = parseInt(caseNumberString);
+
+    const newCaseNumber = previousCaseNumber - 1;
+    caseNumberField.value = newCaseNumber;
+})
