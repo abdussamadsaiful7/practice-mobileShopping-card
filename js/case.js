f209a4f458dd6b9b01f10f6449c1d5e3b75f24d1
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-15 13:32:08 +0600

    added js function

--- js/case.js
+++ js/case.js
@@ -8,7 +8,6 @@ function updateCaseNumber(isIncrease){
 
    let newCaseNumber;
     
-
    if(isIncrease === true){
     newCaseNumber = previousCaseNumber + 1;
    }
@@ -16,28 +15,33 @@ function updateCaseNumber(isIncrease){
     newCaseNumber = previousCaseNumber -1;
    };
    
-  
-
    caseNumberField.value = newCaseNumber;
+   return newCaseNumber;
 };
 
-
-
+function  updateCaseTotalPrice(newCaseNumber){
+    const caseTotalPrice = newCaseNumber * 59;
+   const  caseTotalElement = document.getElementById('case-total');
+   caseTotalElement.innerText = caseTotalPrice;
+}
 
 
 
 document.getElementById('btn-case-plus').addEventListener('click', function(){
-   updateCaseNumber(true);
+   const newCaseNumber = updateCaseNumber(true);
+
+    updateCaseTotalPrice(newCaseNumber);
 });
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
 
-    updateCaseNumber(false);
-    // const caseNumberField = document.getElementById('case-number-field');
-    // const caseNumberString = caseNumberField.value;
-    // const previousCaseNumber  = parseInt(caseNumberString);
+   const newCaseNumber = updateCaseNumber(false);
 
-    // const newCaseNumber2 = previousCaseNumber - 1;
-    // caseNumberField.value = newCaseNumber2;
+
+    updateCaseTotalPrice(newCaseNumber);
+//    const caseTotalPrice = newCaseNumber * 59;
+//    const caseTotalElement = document.getElementById('case-total');
+//    caseTotalElement.innerText = caseTotalPrice;
+    
     
 });
